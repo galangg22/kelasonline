5804843de62e525cd820b2c739c8575d87b215e8
galang <galang@gmail.com>
2025-05-01 14:03:03 +0700

    update

--- resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="text-center">
-        <h1>Selamat Datang di Kelas Online</h1>
-        <p>Belajar dimana saja dan kapan saja!</p>
-        <a href="/classes" class="btn btn-primary">Lihat Kelas</a>
-    </div>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home'); // ← ini diperbaiki!
-});
-
-Route::get('/classes', function () {
-    $classes = [
-        (object)['name' => 'Laravel Dasar', 'description' => 'Belajar Laravel dari 0.'],
-        (object)['name' => 'PHP OOP', 'description' => 'Dasar Pemrograman Berorientasi Objek.'],
-        (object)['name' => 'Web Design', 'description' => 'Membuat tampilan website menarik.'],
-    ];
-    return view('classes.index', compact('classes'));
+    return view('welcome'); // ← ini diperbaiki!
 });
